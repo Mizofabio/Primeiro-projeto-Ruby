8691096d91a1bfcfe0544f51ebd5de24a84b3f46
Mizofabio <yokomizo.fabio@gmail.com>
2023-02-17 21:33:17 -0300

    Envio do primeiro desafio

---
+++ desafio1.rb
@@ -0,0 +1,11 @@
+# Desafio da biblioteca 
+
+print "Bem vindo a biblioteca\n"
+print "\n"
+print "Digite o seu nome: "
+nome = gets.chomp
+print "Digite o seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+puts "Olá #{nome.capitalize} #{sobrenome.capitalize} você tem #{idade} anos." 
